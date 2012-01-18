9c7cd0c050ca6583b51c8310bf15c76a1a96085f
eriforce <eriforce@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-01-18 07:07:44 +0000

    added taskbar manager
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@51 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/Common/TaskbarManager.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PureLib.Common {
+    public class TaskbarManager {
+        private IntPtr _ownerHandle;
+        internal IntPtr OwnerHandle {
+            get {
+                if (_ownerHandle == IntPtr.Zero) {
+                    Process currentProcess = Process.GetCurrentProcess();
+                    if ((currentProcess == null) || (currentProcess.MainWindowHandle == IntPtr.Zero)) {
+                        throw new InvalidOperationException("A valid active Window is needed to update the Taskbar.");
+                    }
+                    _ownerHandle = currentProcess.MainWindowHandle;
+                }
+                return _ownerHandle;
+            }
+        }
+
+        private ITaskbarList4 _taskbarList;
+        internal ITaskbarList4 TaskbarList {
+            get {
+                if (_taskbarList == null) {
+                    lock (this) {
+                        if (_taskbarList == null) {
+                            _taskbarList = (ITaskbarList4)new CTaskbarList();
+                            _taskbarList.HrInit();
+                        }
+                    }
+                }
+                return _taskbarList;
+            }
+        }
+
+        public void SetOverlayIcon(Icon icon, string accessibilityText) {
+            TaskbarList.SetOverlayIcon(OwnerHandle, (icon != null) ? icon.Handle : IntPtr.Zero, accessibilityText);
+        }
+
+        public void SetProgressValue(int currentValue, int maximumValue) {
+            TaskbarList.SetProgressValue(OwnerHandle, Convert.ToUInt32(currentValue), Convert.ToUInt32(maximumValue));
+        }
+
+        public void SetProgressState(TaskbarProgressBarStatus status) {
+            TaskbarList.SetProgressState(OwnerHandle, status);
+        }
+    }
+
+    #region Interop
+
+    [ComImportAttribute()]
+    [GuidAttribute("c43dc798-95d1-4bea-9030-bb99e2983a1a")]
+    [InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface ITaskbarList4 {
+        // ITaskbarList
+        [PreserveSig]
+        void HrInit();
+        [PreserveSig]
+        void AddTab(IntPtr hwnd);
+        [PreserveSig]
+        void DeleteTab(IntPtr hwnd);
+        [PreserveSig]
+        void ActivateTab(IntPtr hwnd);
+        [PreserveSig]
+        void SetActiveAlt(IntPtr hwnd);
+
+        // ITaskbarList2
+        [PreserveSig]
+        void MarkFullscreenWindow(
+            IntPtr hwnd,
+            [MarshalAs(UnmanagedType.Bool)] bool fFullscreen);
+
+        // ITaskbarList3
+        [PreserveSig]
+        void SetProgressValue(IntPtr hwnd, UInt64 ullCompleted, UInt64 ullTotal);
+        [PreserveSig]
+        void SetProgressState(IntPtr hwnd, TaskbarProgressBarStatus tbpFlags);
+        [PreserveSig]
+        void RegisterTab(IntPtr hwndTab, IntPtr hwndMDI);
+        [PreserveSig]
+        void UnregisterTab(IntPtr hwndTab);
+        [PreserveSig]
+        void SetTabOrder(IntPtr hwndTab, IntPtr hwndInsertBefore);
+        [PreserveSig]
+        void SetTabActive(IntPtr hwndTab, IntPtr hwndInsertBefore, uint dwReserved);
+        [PreserveSig]
+        HResult ThumbBarAddButtons(
+            IntPtr hwnd,
+            uint cButtons,
+            [MarshalAs(UnmanagedType.LPArray)] ThumbButton[] pButtons);
+        [PreserveSig]
+        HResult ThumbBarUpdateButtons(
+            IntPtr hwnd,
+            uint cButtons,
+            [MarshalAs(UnmanagedType.LPArray)] ThumbButton[] pButtons);
+        [PreserveSig]
+        void ThumbBarSetImageList(IntPtr hwnd, IntPtr himl);
+        [PreserveSig]
+        void SetOverlayIcon(
+          IntPtr hwnd,
+          IntPtr hIcon,
+          [MarshalAs(UnmanagedType.LPWStr)] string pszDescription);
+        [PreserveSig]
+        void SetThumbnailTooltip(
+            IntPtr hwnd,
+            [MarshalAs(UnmanagedType.LPWStr)] string pszTip);
+        [PreserveSig]
+        void SetThumbnailClip(
+            IntPtr hwnd,
+            IntPtr prcClip);
+
+        // ITaskbarList4
+        void SetTabProperties(IntPtr hwndTab, SetTabPropertiesOption stpFlags);
+    }
+
+    [GuidAttribute("56FDF344-FD6D-11d0-958A-006097C9A090")]
+    [ClassInterfaceAttribute(ClassInterfaceType.None)]
+    [ComImportAttribute()]
+    internal class CTaskbarList { }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    internal struct ThumbButton {
+        /// <summary>
+        /// WPARAM value for a THUMBBUTTON being clicked.
+        /// </summary>
+        internal const int Clicked = 0x1800;
+
+        [MarshalAs(UnmanagedType.U4)]
+        internal ThumbButtonMask Mask;
+        internal uint Id;
+        internal uint Bitmap;
+        internal IntPtr Icon;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        internal string Tip;
+        [MarshalAs(UnmanagedType.U4)]
+        internal ThumbButtonOptions Flags;
+    }
+
+    internal enum SetTabPropertiesOption {
+        None = 0x0,
+        UseAppThumbnailAlways = 0x1,
+        UseAppThumbnailWhenActive = 0x2,
+        UseAppPeekAlways = 0x4,
+        UseAppPeekWhenActive = 0x8
+    }
+
+    internal enum ThumbButtonMask {
+        Bitmap = 0x1,
+        Icon = 0x2,
+        Tooltip = 0x4,
+        THB_FLAGS = 0x8
+    }
+
+    [Flags]
+    internal enum ThumbButtonOptions {
+        Enabled = 0x00000000,
+        Disabled = 0x00000001,
+        DismissOnClick = 0x00000002,
+        NoBackground = 0x00000004,
+        Hidden = 0x00000008,
+        NonInteractive = 0x00000010
+    }
+
+    public enum TaskbarProgressBarStatus {
+        NoProgress = 0,
+        Indeterminate = 0x1,
+        Normal = 0x2,
+        Error = 0x4,
+        Paused = 0x8
+    }
+
+    public enum HResult {
+        /// <summary>     
+        /// S_OK          
+        /// </summary>    
+        Ok = 0x0000,
+
+        /// <summary>
+        /// S_FALSE
+        /// </summary>        
+        False = 0x0001,
+
+        /// <summary>
+        /// E_INVALIDARG
+        /// </summary>
+        InvalidArguments = unchecked((int)0x80070057),
+
+        /// <summary>
+        /// E_OUTOFMEMORY
+        /// </summary>
+        OutOfMemory = unchecked((int)0x8007000E),
+
+        /// <summary>
+        /// E_NOINTERFACE
+        /// </summary>
+        NoInterface = unchecked((int)0x80004002),
+
+        /// <summary>
+        /// E_FAIL
+        /// </summary>
+        Fail = unchecked((int)0x80004005),
+
+        /// <summary>
+        /// E_ELEMENTNOTFOUND
+        /// </summary>
+        ElementNotFound = unchecked((int)0x80070490),
+
+        /// <summary>
+        /// TYPE_E_ELEMENTNOTFOUND
+        /// </summary>
+        TypeElementNotFound = unchecked((int)0x8002802B),
+
+        /// <summary>
+        /// NO_OBJECT
+        /// </summary>
+        NoObject = unchecked((int)0x800401E5),
+
+        /// <summary>
+        /// Win32 Error code: ERROR_CANCELLED
+        /// </summary>
+        Win32ErrorCanceled = 1223,
+
+        /// <summary>
+        /// ERROR_CANCELLED
+        /// </summary>
+        Canceled = unchecked((int)0x800704C7),
+
+        /// <summary>
+        /// The requested resource is in use
+        /// </summary>
+        ResourceInUse = unchecked((int)0x800700AA),
+
+        /// <summary>
+        /// The requested resources is read-only.
+        /// </summary>
+        AccessDenied = unchecked((int)0x80030005)
+    }
+
+    #endregion
+}
