5d7416bd1348fec841f1da635caa53c3627124e1
eriforce <eriforce@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-03-16 02:53:13 +0000

    added view model base for WPF apps
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@61 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/WPF/ViewModelBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace PureLib.WPF {
+    public abstract class ViewModelBase : INotifyPropertyChanged {
+        private readonly Dispatcher uiDispatcher;
+
+        public Window View { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModelBase()
+            : this(Dispatcher.CurrentDispatcher) {
+        }
+
+        public ViewModelBase(Dispatcher uiDispatcher) {
+            this.uiDispatcher = uiDispatcher;
+        }
+
+        protected void SetProperty<T>(ref T storage, T val, params string[] fieldsToNotify) {
+            storage = val;
+            foreach (var field in fieldsToNotify)
+                RaiseChange(field);
+        }
+
+        protected void RaiseChange(string fieldName) {
+            if (PropertyChanged == null)
+                return;
+
+            Action<string> action = (t) => {
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(t));
+            };
+
+            uiDispatcher.BeginInvoke(action, fieldName);
+        }
+
+        public void RunOnUIThread(Action code) {
+            uiDispatcher.BeginInvoke(code);
+        }
+    }
+}
