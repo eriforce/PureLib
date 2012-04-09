b17596aa5f63dfa69f0ea56c8a3d1a8583780113
eriforce <eriforce@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-04-09 05:52:41 +0000

    added wpf extensions
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@67 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/WPF/WpfExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace PureLib.WPF {
+    public static class WpfExtensions {
+        public static void AddRange<T>(this ObservableCollection<T> list, IEnumerable<T> items) {
+            if ((list == null) || items == null)
+                throw new ArgumentNullException();
+            foreach (T item in items)
+                list.Add(item);
+        }
+    }
+}
