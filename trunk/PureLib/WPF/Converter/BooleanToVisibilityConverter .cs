895cfbeac57bccdf84303f4fdc1216ab1f47e727
eriforce <eriforce@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-04-11 15:11:53 +0000

    added BooleanToVisibilityConverter
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@68 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/WPF/Converter/BooleanToVisibilityConverter .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace PureLib.WPF {
+    public class BooleanToVisibilityConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            bool isVisible = bool.Parse(value.ToString());
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
