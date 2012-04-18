d6dae646b6781c9278e963c097be84accdae6c63
eriforce <eriforce@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-04-18 04:36:06 +0000

    added NotifyObject
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@72 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/WPF/NotifyObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using PureLib.Properties;
+
+namespace PureLib.WPF {
+    public class NotifyObject : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaiseChange<T>(Expression<Func<T>> propertyExpression) {
+            if (propertyExpression == null)
+                throw new ArgumentNullException("propertyExpression");
+
+            MemberExpression memberExpression = propertyExpression.Body as MemberExpression;
+            if (memberExpression == null)
+                throw new ArgumentException(Resources.ViewModelBase_NotMemberAccessExpression_Exception, "propertyExpression");
+
+            PropertyInfo property = memberExpression.Member as PropertyInfo;
+            if (property == null)
+                throw new ArgumentException(Resources.ViewModelBase_ExpressionNotProperty_Exception, "propertyExpression");
+
+            MethodInfo getMethod = property.GetGetMethod(true);
+            if (getMethod.IsStatic)
+                throw new ArgumentException(Resources.ViewModelBase_StaticExpression_Exception, "propertyExpression");
+
+            RaiseChange(memberExpression.Member.Name);
+        }
+
+        protected void RaiseChange(string fieldName) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(fieldName));
+        }
+    }
+}
--- trunk/PureLib/WPF/ViewModelBase.cs
+++ trunk/PureLib/WPF/ViewModelBase.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Threading;
 using PureLib.Properties;
 
 namespace PureLib.WPF {
-    public abstract class ViewModelBase : INotifyPropertyChanged {
+    public abstract class ViewModelBase : NotifyObject {
         private readonly Dispatcher uiDispatcher;
 
         public Window View { get; set; }
-        public event PropertyChangedEventHandler PropertyChanged;
 
         public ViewModelBase()
             : this(Dispatcher.CurrentDispatcher) {
@@ -24,44 +21,11 @@ namespace PureLib.WPF {
             this.uiDispatcher = uiDispatcher;
         }
 
-        protected void SetProperty<T>(ref T storage, T val, params string[] fieldsToNotify) {
-            storage = val;
-            foreach (var field in fieldsToNotify)
-                RaiseChange(field);
+        public void RunOnUIThread(Action code) {
+            uiDispatcher.BeginInvoke(code);
         }
 
-        protected void RaiseChange<T>(Expression<Func<T>> propertyExpression) {
-            if (propertyExpression == null)
-                throw new ArgumentNullException("propertyExpression");
-
-            MemberExpression memberExpression = propertyExpression.Body as MemberExpression;
-            if (memberExpression == null)
-                throw new ArgumentException(Resources.ViewModelBase_NotMemberAccessExpression_Exception, "propertyExpression");
-
-            PropertyInfo property = memberExpression.Member as PropertyInfo;
-            if (property == null)
-                throw new ArgumentException(Resources.ViewModelBase_ExpressionNotProperty_Exception, "propertyExpression");
-
-            MethodInfo getMethod = property.GetGetMethod(true);
-            if (getMethod.IsStatic)
-                throw new ArgumentException(Resources.ViewModelBase_StaticExpression_Exception, "propertyExpression");
-
-            RaiseChange(memberExpression.Member.Name);
-        }
-
-        protected void RaiseChange(string fieldName) {
-            if (PropertyChanged == null)
-                return;
-
-            Action<string> action = (t) => {
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs(t));
-            };
-
-            uiDispatcher.BeginInvoke(action, fieldName);
-        }
-
-        protected void WorkInBackground(DoWorkEventHandler work, RunWorkerCompletedEventHandler onCompleted = null) {
+        protected void RunInBackground(DoWorkEventHandler work, RunWorkerCompletedEventHandler onCompleted = null) {
             if (uiDispatcher == null)
                 work.Invoke(null, new DoWorkEventArgs(null));
             else {
@@ -74,10 +38,6 @@ namespace PureLib.WPF {
                     worker.RunWorkerAsync();
                 }
             }
-        }
-
-        public void RunOnUIThread(Action code) {
-            uiDispatcher.BeginInvoke(code);
         }
     }
 }
