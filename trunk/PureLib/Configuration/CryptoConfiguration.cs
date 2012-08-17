53afe1e1b45afbe49e80a0cc78140023b577d749
Fire <Fire@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-08-17 09:11:54 +0000

    added RSA stuffs to CryptoConfiguration
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@104 4dedb8ab-72de-3345-9651-2401c5e3755d

--- trunk/PureLib/Configuration/CryptoConfiguration.cs
+++ trunk/PureLib/Configuration/CryptoConfiguration.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using PureLib.Common;
 
@@ -9,17 +11,20 @@ namespace PureLib.Configuration {
     public class CryptoConfiguration : ConfigurationElement {
         private const string iv = "iv";
         private const string key = "key";
+        private const string thumbprint = "thumbprint";
+        private const string storeName = "storeName";
+        private const string storeLocation = "storeLocation";
 
         public byte[] IV {
-            get {
-                return IvString.IsNullOrEmpty() ?
-                    null : IvString.FromBase64String();
-            }
+            get { return GetBytesFromString(IvString); }
         }
         public byte[] Key {
+            get { return GetBytesFromString(KeyString); }
+        }
+        public X509Certificate2 Certificate {
             get {
-                return KeyString.IsNullOrEmpty() ?
-                    null : KeyString.FromBase64String();
+                return Thumbprint.IsNullOrEmpty() ? null :
+                    CryptographyHelper.GetCertificate(StoreName, StoreLocation, X509FindType.FindByThumbprint, Thumbprint);
             }
         }
 
@@ -31,6 +36,31 @@ namespace PureLib.Configuration {
         [ConfigurationProperty(key, IsRequired = true)]
         public string KeyString {
             get { return (string)this[key]; }
+        }
+
+        [ConfigurationProperty(thumbprint)]
+        public string Thumbprint {
+            get { return (string)this[thumbprint]; }
+        }
+
+        [ConfigurationProperty(storeName, DefaultValue = StoreName.My)]
+        public StoreName StoreName {
+            get { return (StoreName)this[storeName]; }
+        }
+
+        [ConfigurationProperty(storeLocation, DefaultValue = StoreLocation.LocalMachine)]
+        public StoreLocation StoreLocation {
+            get { return (StoreLocation)this[storeLocation]; }
+        }
+
+        private byte[] GetBytesFromString(string s) {
+            if (s.IsNullOrEmpty())
+                return null;
+
+            byte[] bin = s.FromBase64String();
+            if (!Thumbprint.IsNullOrEmpty())
+                bin = bin.Decrypt((RSACryptoServiceProvider)Certificate.PrivateKey);
+            return bin;
         }
     }
 }
