764e08adfa9b9d6bcc3d63a86c831e6ff7b78c4c
Fire <Fire@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-11-29 08:08:30 +0000

    updated chinese conversion
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@114 4dedb8ab-72de-3345-9651-2401c5e3755d

--- trunk/PureLib/Common/LanguageHelper.cs
+++ trunk/PureLib/Common/LanguageHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -9,6 +10,15 @@ using Microsoft.VisualBasic;
 
 namespace PureLib.Common {
     public static class LanguageHelper {
+        private static ChineseConverter _converter;
+        private static ChineseConverter _loadedConverter {
+            get {
+                if (_converter == null)
+                    _converter = new ChineseConverter();
+                return _converter;
+            }
+        }
+
         public static string ResolveUnreadableCodes(this string text, Encoding src, Encoding dst) {
             return dst.GetString(src.GetBytes(text));
         }
@@ -26,11 +36,11 @@ namespace PureLib.Common {
         }
 
         public static string ToSimplifiedChinese(this string traditional) {
-            return Strings.StrConv(traditional, VbStrConv.SimplifiedChinese);
+            return _loadedConverter.ToSimplifiedChinese(traditional);
         }
 
         public static string ToTraditionalChinese(this string simplified) {
-            return Strings.StrConv(simplified, VbStrConv.TraditionalChinese);
+            return _loadedConverter.ToTraditionalChinese(simplified);
         }
 
         public static string ToKatakana(this string hiragana) {
@@ -43,6 +53,40 @@ namespace PureLib.Common {
 
         public static string ParseRomaji(this string romaji) {
             return KanaConverter.RomajiToHiragana(romaji);
+        }
+
+        private class ChineseConverter {
+            private const string additionalTcScPairName = "additionalTcScPair";
+            private const char pairsSeparator = ',';
+            private const char pairSeparator = ':';
+            private Dictionary<string, string> tcScMaps = new Dictionary<string, string>();
+
+            public ChineseConverter() {
+                string pairString = ConfigurationManager.AppSettings[additionalTcScPairName];
+                if (!pairString.IsNullOrEmpty()) {
+                    string[] scTcPairs = pairString.Split(pairsSeparator);
+                    foreach (string scTcPair in scTcPairs) {
+                        string[] parts = scTcPair.Split(pairSeparator);
+                        tcScMaps.Add(parts.First(), parts.Last());
+                    }
+                }
+            }
+
+            public string ToSimplifiedChinese(string traditional) {
+                string sc = Strings.StrConv(traditional, VbStrConv.SimplifiedChinese);
+                foreach (var p in tcScMaps) {
+                    sc = sc.Replace(p.Key, p.Value);
+                }
+                return sc;
+            }
+
+            public string ToTraditionalChinese(string simplified) {
+                string tc = Strings.StrConv(simplified, VbStrConv.TraditionalChinese);
+                foreach (var p in tcScMaps) {
+                    tc = tc.Replace(p.Value, p.Key);
+                }
+                return tc;
+            }
         }
     }
 }
