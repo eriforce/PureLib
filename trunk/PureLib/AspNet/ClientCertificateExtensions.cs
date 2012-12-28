f641c6ce472c641ba2917d46e15bed4896701913
Fire <Fire@4dedb8ab-72de-3345-9651-2401c5e3755d>
2012-12-28 06:23:38 +0000

    added ClientCertificateExtensions
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@119 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/AspNet/ClientCertificateExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Security;
+using PureLib.Common;
+
+namespace PureLib.AspNet {
+    public static class ClientCertificateExtensions {
+        public static bool Authenticate(this HttpClientCertificate certificate, string issuer = null) {
+            if (Debugger.IsAttached)
+                FormsAuthentication.SetAuthCookie("Debug", false);
+
+            if (ValidateClientCertificate(certificate, issuer))
+                FormsAuthentication.SetAuthCookie(certificate.Subject.Substring("CN=".Length), false);
+            else
+                return false;
+
+            return true;
+        }
+
+        private static bool ValidateClientCertificate(HttpClientCertificate cert, string issuer) {
+            if ((cert == null) || !cert.IsValid)
+                return false;
+
+            if (!issuer.IsNullOrEmpty() && !cert.BinaryIssuer.ToHexString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            return true;
+        }
+    }
+}
