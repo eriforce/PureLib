b463f41fdaa8fa7683b0c8dfcbee5652204cf739
Fire <Fire@4dedb8ab-72de-3345-9651-2401c5e3755d>
2013-02-16 02:02:19 +0000

    added configuration section, won't copy JP converter by default
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@126 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/Configuration/PureLibConfigurationSection.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace PureLib.Configuration {
+    public class PureLibConfigurationSection : ConfigurationSection {
+        private const string mailConfiguration = "mailConfiguration";
+        private const string cryptoConfiguration = "cryptoConfiguration";
+
+        public const string Name = "purelibConfiguration";
+
+        [ConfigurationProperty(mailConfiguration)]
+        public MailConfiguration MailConfiguration {
+            get { return (MailConfiguration)this[mailConfiguration]; }
+            set { this[mailConfiguration] = value; }
+        }
+
+        [ConfigurationProperty(cryptoConfiguration)]
+        public CryptoConfiguration CryptoConfiguration {
+            get { return (CryptoConfiguration)this[cryptoConfiguration]; }
+            set { this[cryptoConfiguration] = value; }
+        }
+    }
+}
