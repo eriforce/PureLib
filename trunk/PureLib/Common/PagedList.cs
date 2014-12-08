99d1034c430dba71da720117dd90a93b58c4b82a
Erich <Erich@4dedb8ab-72de-3345-9651-2401c5e3755d>
2014-12-08 07:27:31 +0000

    added paged list
    git-svn-id: https://eriforce.eicp.net:8443/svn/PureLib@138 4dedb8ab-72de-3345-9651-2401c5e3755d

---
+++ trunk/PureLib/Common/PagedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PureLib.Common {
+    public class PagedList<T> : List<T> {
+        public int PageSize { get; private set; }
+        public int Page { get; private set; }
+        public int TotalCount { get; private set; }
+        public int StartIndex {
+            get { return (Page - 1) * PageSize; }
+        }
+        public int TotalPage {
+            get {
+                if ((TotalCount % PageSize) > 0)
+                    return (TotalCount / PageSize) + 1;
+                else
+                    return TotalCount / PageSize;
+            }
+        }
+
+        public PagedList(IEnumerable<T> pagedList, int page, int pageSize, int totalCount)
+            : base(pagedList) {
+            Page = page;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+        }
+
+        public PagedList(IQueryable<T> query, int page, int pageSize)
+            : base(query.Skip((page - 1) * pageSize).Take(pageSize)) {
+            Page = page;
+            PageSize = pageSize;
+            TotalCount = query.Count();
+        }
+
+        public PagedList<TResult> Cast<TResult>() where TResult : class {
+            return new PagedList<TResult>(this.Select(p => p as TResult), Page, PageSize, TotalCount);
+        }
+    }
+
+    public static class PagedListExtension {
+        public static PagedList<T> Page<T>(this IQueryable<T> query, int page, int pageSize) {
+            return new PagedList<T>(query, page, pageSize);
+        }
+
+        public static PagedList<T> Page<T>(this IEnumerable<T> data, int page, int pageSize, int totalCount) {
+            return new PagedList<T>(data, page, pageSize, totalCount);
+        }
+    }
+}
