3e9947d38d41ccb0440c0d4cbb2ee632738a91d0
Erich <eriforce@live.com>
2016-04-16 16:00:54 +0800

    updated PathWrapper

--- PureLib/Common/PathWrapper.cs
+++ PureLib/Common/PathWrapper.cs
@@ -8,25 +8,22 @@ using System.Text.RegularExpressions;
 
 namespace PureLib.Common {
     public static class PathWrapper {
-        private const string invalidPathPattern = "[\":/<>\\?\\*\\|]";
-        private static readonly string escapedDirectorySeparator = "{0}{0}".FormatWith(Path.DirectorySeparatorChar);
-
-        public static string FilterInvalidChar(this string path, string replacement = "_") {
-            return Regex.Replace(path, invalidPathPattern, replacement);
+        public static string FilterInvalidChar(this string path, string replacement = "_", string additionalInvalid = "*?") {
+            string pattern = "[{0}]".FormatWith(string.Join(string.Empty,
+                Path.GetInvalidPathChars().Concat(additionalInvalid).Distinct().Select(c => Regex.Escape(c.ToString()))));
+            return Regex.Replace(path, pattern, replacement);
         }
 
-        public static string WrapPath(this string path) {
-            if (Regex.IsMatch(path, @"^([a-zA-Z]:|\\)?\\"))
-                return path;
-            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+        public static string MakeFullPath(this string path) {
+            return Path.IsPathRooted(path) ? path : Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
         }
 
-        public static string CombineWithAppName(this string ext) {
-            return Path.ChangeExtension(Process.GetCurrentProcess().MainModule.FileName, ext).WrapPath();
+        public static string GetAppPath(string ext) {
+            return Path.ChangeExtension(Process.GetCurrentProcess().MainModule.FileName, ext);
         }
 
         public static string EscapePathForCmd(this string path) {
-            return path.Replace(Path.DirectorySeparatorChar.ToString(), escapedDirectorySeparator);
+            return path.Replace(Path.DirectorySeparatorChar.ToString(), new string(Path.DirectorySeparatorChar, 2));
         }
     }
 }
