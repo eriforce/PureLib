0158c0da57220bfc3752df4366638c9bd63ec96d
Erich Yu <eriforce@live.com>
2016-12-06 23:49:51 -0500

    moved paged list to PureLib

--- PureLib.EntityFramework/PagedList.cs
+++ PureLib.EntityFramework/PagedList.cs
@@ -3,41 +3,10 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
+using PureLib.Common;
 
 namespace PureLib.EntityFramework {
-    public class PagedList<T> : List<T> {
-        public int PageSize { get; private set; }
-        public int Page { get; private set; }
-        public int TotalCount { get; private set; }
-        public int StartIndex {
-            get { return (Page - 1) * PageSize; }
-        }
-        public int TotalPage {
-            get {
-                if ((TotalCount % PageSize) > 0)
-                    return (TotalCount / PageSize) + 1;
-                else
-                    return TotalCount / PageSize;
-            }
-        }
-
-        public PagedList(IEnumerable<T> pagedList, int page, int pageSize, int totalCount)
-            : base(pagedList) {
-            Page = page;
-            PageSize = pageSize;
-            TotalCount = totalCount;
-        }
-
-        public PagedList<TResult> Cast<TResult>() where TResult : class {
-            return new PagedList<TResult>(this.Select(p => p as TResult), Page, PageSize, TotalCount);
-        }
-    }
-
     public static class PagedListExtension {
-        public static PagedList<T> AttachPagination<T>(this IEnumerable<T> data, int page, int pageSize, int totalCount) {
-            return new PagedList<T>(data, page, pageSize, totalCount);
-        }
-
         public static PagedList<T> Paginate<T>(this IQueryable<T> query, int page, int pageSize) {
             int count = query.Count();
             return query
