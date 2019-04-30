05b3557b2b24e864b432d713a96edf6e21a1608e
Erich Yu <eriforce@live.com>
2019-04-30 11:48:03 +0800

    added MiddlewareContainer

---
+++ PureLib/Common/MiddlewareContainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureLib.Common {
+    public class MiddlewareContainer<T> where T : class {
+        public delegate Task NextDelegate(T context);
+
+        private IList<Func<NextDelegate, NextDelegate>> _middlewares;
+
+        public MiddlewareContainer() {
+            _middlewares = new List<Func<NextDelegate, NextDelegate>>();
+        }
+
+        public NextDelegate Compose() {
+            NextDelegate next = context => Task.CompletedTask;
+
+            foreach (var middleware in _middlewares.Reverse()) {
+                next = middleware(next);
+            }
+
+            return next;
+        }
+
+        public MiddlewareContainer<T> Use(Func<T, Func<Task>, Task> middleware) {
+            return Use(next => {
+                return context => {
+                    Func<Task> simpleNext = () => next(context);
+                    return middleware(context, simpleNext);
+                };
+            });
+        }
+
+        private MiddlewareContainer<T> Use(Func<NextDelegate, NextDelegate> middleware) {
+            _middlewares.Add(middleware);
+            return this;
+        }
+    }
+}
