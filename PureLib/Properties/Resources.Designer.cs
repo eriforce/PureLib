b79ea2223bf59a8c3a7fa1c52f9ddc73365c3878
Erich Yu <eriforce@live.com>
2022-12-22 13:11:18 +0800

    cleanup text resources

--- PureLib/Properties/Resources.Designer.cs
+++ PureLib/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace PureLib.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -111,15 +111,6 @@ namespace PureLib.Properties {
         internal static string Common_Seconds {
             get {
                 return ResourceManager.GetString("Common_Seconds", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to -: Inner Exception :-.
-        /// </summary>
-        internal static string ExceptionHandling_InnerException {
-            get {
-                return ResourceManager.GetString("ExceptionHandling_InnerException", resourceCulture);
             }
         }
         
