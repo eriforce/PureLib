317009736493cbe45fc33a1bc54ed8bc732a356a
Erich Yu <eriforce@live.com>
2023-01-28 15:03:50 +0800

    rewrote Arguments

---
+++ PureLib.Tests/ArgumentsTest.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.ObjectModel;
+using PureLib.Common;
+using Shouldly;
+
+namespace PureLib.Tests {
+    public class ArgumentsTest {
+        private readonly Arguments _arguments = new Arguments(new[] {
+            "-f", "format", "/d", "100.0", "dd", "--delay", "-1000",
+        });
+
+        [Theory]
+        [InlineData("f", true)]
+        [InlineData("d", true)]
+        [InlineData("delay", true)]
+        [InlineData("format", false)]
+        public void Arguments_Contains_ShouldReturnTrueIfKeyExists(string key, bool expectedExists) {
+            _arguments.Contains(key).ShouldBe(expectedExists);
+        }
+
+        [Theory]
+        [InlineData("f", "format", true)]
+        [InlineData("d", "100.0", true)]
+        [InlineData("delay", "-1000", true)]
+        [InlineData("format", null, false)]
+        public void Arguments_TryGetValue_ShouldGetValue(string key, string expectedValue, bool expectedExists) {
+            _arguments.TryGetValue(key, out var value).ShouldBe(expectedExists);
+            value.ShouldBe(expectedValue);
+        }
+
+        [Theory]
+        [InlineData("f", "format", true)]
+        [InlineData("d", "100.0", true)]
+        [InlineData("delay", "-1000", true)]
+        [InlineData("format", "test", false)]
+        public void Arguments_GetValueOrDefault_ShouldGetValue(string key, string expectedValue, bool expectedExists) {
+            bool exists;
+            string value = _arguments.Contains(key) ?
+                _arguments.GetValueOrDefault(key, out exists) :
+                _arguments.GetValueOrDefault(key, out exists, expectedValue);
+
+            exists.ShouldBe(expectedExists);
+            value.ShouldBe(expectedValue);
+        }
+
+        [Fact]
+        public void Arguments_GetValueOrDefaultGeneric_ShouldGetValue() {
+            bool exists;
+
+            _arguments.GetValueOrDefault("d", out exists, double.Parse, 10).ShouldBe(100.0);
+            exists.ShouldBeTrue();
+            _arguments.GetValueOrDefault("dd", out exists, double.Parse, 10).ShouldBe(10.0);
+            exists.ShouldBeFalse();
+
+            _arguments.GetValueOrDefault("delay", out exists, int.Parse, 10).ShouldBe(-1000);
+            exists.ShouldBeTrue();
+            _arguments.GetValueOrDefault("delay2", out exists, int.Parse, -100).ShouldBe(-100);
+            exists.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Arguments_TryGetValues_ShouldGetValues() {
+            ReadOnlyCollection<string> values;
+
+            _arguments.TryGetValues("f", out values).ShouldBeTrue();
+            values.ShouldBe(new[] { "format" });
+
+            _arguments.TryGetValues("d", out values).ShouldBeTrue();
+            values.ShouldBe(new[] { "100.0", "dd" });
+
+            _arguments.TryGetValues("dd", out values).ShouldBeFalse();
+            values.ShouldBeNull();
+        }
+    }
+}
--- PureLib/Common/Arguments.cs
+++ PureLib/Common/Arguments.cs
@@ -1,34 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace PureLib.Common {
-    public sealed class Arguments : Dictionary<string, List<string>> {
+    public sealed class Arguments {
         private const string ARGUMENT_NAME = "name";
         private const string _argumentPattern = $$"""^(/|\-{1,2})(?<{{ARGUMENT_NAME}}>\w+)$""";
         private static readonly Regex _argumentRegex = new(_argumentPattern, RegexOptions.Compiled | RegexOptions.NonBacktracking);
 
-        public Arguments(string[] args) : base(StringComparer.OrdinalIgnoreCase) {
-            if ((args == null) || (args.Length == 0))
-                return;
+        private readonly Dictionary<string, ReadOnlyCollection<string>> _internal;
 
-            string currentName = null;
+        public Arguments(string[] args, StringComparer stringComparer = null) {
+            _internal = new Dictionary<string, ReadOnlyCollection<string>>(stringComparer);
+
+            List<string> currentArgument = null;
             foreach (string arg in args) {
                 Match m = _argumentRegex.Match(arg);
                 if (m.Success && !decimal.TryParse(arg, out decimal argValue)) {
-                    currentName = m.Groups[ARGUMENT_NAME].Value;
-                    Add(currentName, new List<string>());
+                    string currentName = m.Groups[ARGUMENT_NAME].Value;
+                    currentArgument = new List<string>();
+                    _internal.Add(currentName, currentArgument.AsReadOnly());
                 }
-                else if (!currentName.IsNullOrEmpty())
-                    this[currentName].Add(arg);
+                else if (currentArgument != null)
+                    currentArgument.Add(arg);
             }
         }
 
-        public string GetValue(string key) {
-            if (!ContainsKey(key))
-                throw new KeyNotFoundException("The key cannot be found in arguments.");
-            return this[key].FirstOrDefault();
+        public bool Contains(string key) {
+            return _internal.ContainsKey(key);
+        }
+
+        public bool TryGetValue(string key, out string value) {
+            bool exists = TryGetValues(key, out ReadOnlyCollection<string> values);
+            value = values?.FirstOrDefault();
+            return exists;
+        }
+
+        public string GetValueOrDefault(string key, out bool exists, string defaultValue = default) {
+            exists = TryGetValues(key, out ReadOnlyCollection<string> values);
+            return exists ? values.FirstOrDefault() : defaultValue;
+        }
+
+        public T GetValueOrDefault<T>(string key, out bool exists, Func<string, T> parseFunc, T defaultValue = default) {
+            exists = TryGetValues(key, out ReadOnlyCollection<string> values);
+            return exists && values.Any() ? parseFunc(values.First()) : defaultValue;
+        }
+
+        public bool TryGetValues(string key, out ReadOnlyCollection<string> values) {
+            ref ReadOnlyCollection<string> list = ref CollectionsMarshal.GetValueRefOrNullRef(_internal, key);
+            bool exists = !Unsafe.IsNullRef(ref list);
+            values = exists ? list : null;
+            return exists;
         }
     }
 }
